7323870f1204127365a632271b1d827c60880337
mikedelcastillo <johnmichaeldc@gmail.com>
2023-04-23 12:15:12 +0000

    feat: sending socket to serial

--- packages/api/src/helpers.ts
+++ packages/api/src/helpers.ts
@@ -3,12 +3,12 @@ import { COMMS_FLOAT_BYTE_COUNT, COMMS_FLOAT_NEG_BYTE, COMMS_FLOAT_POS_BYTE } fr
 export const rangeToByte = (range: number): string => {
     range = Math.max(-1, Math.min(1, range))
     const sign = Math.sign(range)
-    const index = Math.round(Math.abs(range) * (COMMS_FLOAT_BYTE_COUNT - 1))
+    const index = Math.round(Math.abs(range) * (COMMS_FLOAT_BYTE_COUNT))
     if(index !== 0){
         return String.fromCharCode(
             sign > 0 ?
-                COMMS_FLOAT_POS_BYTE + index :
-                COMMS_FLOAT_NEG_BYTE + index
+                COMMS_FLOAT_POS_BYTE + index - 1 :
+                COMMS_FLOAT_NEG_BYTE + index - 1
         )
     }
     return "0"
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,8 +3,27 @@ import { DEFAULT_PORT } from "./constants"
 
 const wss = new WebSocketServer({ port: DEFAULT_PORT })
 
+import { SerialPort, ReadlineParser } from "serialport"
+import { rangeToByte } from "./helpers"
+
+const port = new SerialPort({
+    path: "/dev/ttyACM0",
+    baudRate: 115200,
+})
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+
+parser.on("data", (data) => {
+    // console.log(data)
+})
+
 wss.on("connection", (ws) => {
     ws.on("message", (data) => {
-        console.log(data)
+        const input = Number(data.toString())
+        const char = rangeToByte(input)
+        console.log(input, char)
+        port.write(char)
     })
 })
+
