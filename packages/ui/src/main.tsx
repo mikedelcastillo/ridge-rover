9607114026e61f07c328d003c29a74fbb4fd6523
mikedelcastillo <johnmichaeldc@gmail.com>
2023-04-23 12:17:24 +0000

    feat: basic steering with mouse

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -9,11 +9,18 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>,
 )
 
-const { hostname } = window.location
-const wsUrl = `ws://${hostname}:5000`
+const { hostname, hash } = window.location
+const wsUrl = `ws://${hash.substring(1) || hostname}:5000`
+
+window.addEventListener("mousemove", (event) => {
+  const curX = event.pageX
+  const w = window.innerWidth / 2
+  const value = (curX - w) / w
+  ws.send(value.toFixed(2))
+})
 
 const ws = new WebSocket(wsUrl)
-console.log("connecting...")
+console.log("connecting...", wsUrl)
 ws.addEventListener("open", () => {
   console.log("connected!")
 })
